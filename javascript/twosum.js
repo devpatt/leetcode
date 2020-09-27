5c46c4be660faef0680128523e41053643a717d1
Deven <devenpattni@hotmail.com>
2020-09-27 03:26:04 -0400

    updated twosum to make it O(n)

--- javascript/twosum.js
+++ javascript/twosum.js
@@ -3,23 +3,16 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
-    var numElem = nums.length;
-    
-    var numOne;
-    var numTwo;
-    
-    var a = 0;
-     
-    for (a; a < numElem; a++){
-        var b = 0;
-        for (b; b < numElem; b++){
-            
-            if ((nums[a] + nums[b]) === target && a !== b){
-                return [a,b];
-            }
+var twoSum = function (nums, target) {
+  let numsObj = {}; // makes an object/map with desired numbers as "keys", and indices as the "value"
 
-        }
+  // loop through array once, checks if a desird value was found (then done)
+  for (let i = 0; i < nums.length; i++) {
+    if (numsObj[nums[i]] != undefined) {
+      return [numsObj[nums[i]], i];
     }
-    
+    numsObj[target - nums[i]] = i; // stores the current index with the key of the future desirded value (target - current number)
+  }
+
+  // O(n) efficiency
 };
